82cc5d683804c1da74232d59bb4b1767dd54c7d7
unknown <Echu@Echu_PC.local>
2021-04-06 16:52:05 -0400

    first commit

---
+++ src/config.js
@@ -0,0 +1,9 @@
+export default {
+  
+    apiKey: 
+    authDomain: "chat-app-f15fd.firebaseapp.com",
+    projectId: "chat-app-f15fd",
+    storageBucket: "chat-app-f15fd.appspot.com",
+    messagingSenderId: "277777545792",
+    appId: "1:277777545792:web:f13fe8409b9f8300d97da5"
+};
